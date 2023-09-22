6f9776f2b659200b743b35916a363fc4c847507e
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-09-22 15:31:35 +0700

    modify salary and salary deduction

--- Practice/Week 4/Salary24.java
+++ Practice/Week 4/Salary24.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Salary24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int numAttendance, numAbsence, totalSalary;
-        int salary=40000, salaryDeduction=25000;
+        int numAttendance, numAbsence, totalSalary, salary, salaryDeduction;
 
         System.out.println("Input attendance number: ");
         numAttendance = input.nextInt();
         System.out.println("Input absence number: ");
         numAbsence = input.nextInt();
+        System.out.println("Input salary: ");
+        salary = input.nextInt();
+        System.out.println("Input salary deduction: ");
+        salaryDeduction = input.nextInt();
 
         totalSalary = (numAttendance*salary)-(numAbsence*salaryDeduction);
         System.out.println("Total salary: " + totalSalary);

12b86a4da07dcd199cecd9a115c7af118deee70b
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-09-22 11:37:36 +0700

    modify variable phi

--- Practice/Week 4/Circle24.java
+++ Practice/Week 4/Circle24.java
@@ -4,12 +4,12 @@ public class Circle24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int r;
-        double circumference, area;
+        double phi=3.14, circumference, area;
 
         System.out.println("Input radius: ");
         r = input.nextInt();
-        area = 3.14*r*r;
-        circumference = 2*3.14*r;
+        area = phi*r*r;
+        circumference = 2*phi*r;
         System.out.println("Area: " + area); 
         System.out.println("Circumference: " + circumference);
     }

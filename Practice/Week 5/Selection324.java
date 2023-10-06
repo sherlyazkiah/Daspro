876344b9c093c1583181c0983be5812433f5aaaa
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-10-06 13:04:06 +0700

    commit

--- Practice/Week 5/Selection324.java
+++ Practice/Week 5/Selection324.java
@@ -20,6 +20,7 @@ public class Selection324 {
                 break;
             case '-':
                 result = operand1 - operand2;
+                
                 System.out.println(operand1 + " - " + operand2 + " = " +result);
                 break;
             case '*':

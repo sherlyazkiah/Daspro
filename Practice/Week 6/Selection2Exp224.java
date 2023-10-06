abd2f808fcb95f1d6893b25a5ed7f60f7ab8b9f4
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-10-06 13:27:31 +0700

    add triangle types

--- Practice/Week 6/Selection2Exp224.java
+++ Practice/Week 6/Selection2Exp224.java
@@ -14,12 +14,18 @@ public class Selection2Exp224 {
 
         totalAngle = angle1+angle2+angle3;
         
-        if(totalAngle == 180) 
-            if(angle1==90 || angle2==90 || angle3==90)
+        if(totalAngle == 180) {
+            if(angle1==90 || angle2==90 || angle3==90) {
                 System.out.println("Right triangle");
-            else
+            } else if(angle1==angle2 && angle2==angle3) {
+                System.out.println("Equilateral triangle");
+            } else if(angle1==angle2 || angle2==angle3 || angle1==angle3) {
+                System.out.println("Isosceles triangle");
+            } else {
                 System.out.println("Not a right triangle");
-        else
+            }     
+        } else {
             System.out.println("Not a triangle");
+        }     
     }
 }

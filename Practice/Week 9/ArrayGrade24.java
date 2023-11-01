9f7407273310970810c8b1c6306a3df5d7cbc3d5
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-01 12:59:28 +0700

    new file

---
+++ Practice/Week 9/ArrayGrade24.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArrayGrade24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] finalGrade = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Input final grade "+i+" : ");
+            finalGrade[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Final grade "+i+" is "+finalGrade[i]);
+        }
+    }
+}

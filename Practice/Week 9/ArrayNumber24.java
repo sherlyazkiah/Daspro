7b985628eb0fa29630f93e75112d2132fe7e099b
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-01 12:39:44 +0700

    new file

---
+++ Practice/Week 9/ArrayNumber24.java
@@ -0,0 +1,14 @@
+public class ArrayNumber24 {
+    public static void main(String[] args) {
+        int[] number = new int[4];
+        number[0] = 5;
+        number[1] = 13;
+        number[2] = -7;
+        number[3] = 17;
+
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+        System.out.println(number[3]);
+    } 
+}

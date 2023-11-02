725974acc3d45a0141296d252301b1c77ca645b5
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-02 21:49:01 +0700

    inisialisasi nilai array secara langsung

--- Practice/Week 9/ArrayNumber24.java
+++ Practice/Week 9/ArrayNumber24.java
@@ -1,12 +1,8 @@
 public class ArrayNumber24 {
     public static void main(String[] args) {
-        double[] number = new double[4];
-        number[0] = 5.0;
-        number[1] = 12867;
-        number[2] = 7.5;
-        number[3] = 2000000;
+        int[] number = {5, 12867, 75, 2000000}; //inisialisasi nilai array secara langsung
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i <= 4; i++) {
             System.out.println(number[i]);
         }
     } 

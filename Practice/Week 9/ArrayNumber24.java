e37060d414858c59e77af1c6e7165fcdebfa08fe
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-02 23:36:22 +0700

    change variable

--- Practice/Week 9/ArrayNumber24.java
+++ Practice/Week 9/ArrayNumber24.java
@@ -1,8 +1,8 @@
 public class ArrayNumber24 {
     public static void main(String[] args) {
-        int[] number = {5, 12867, 75, 2000000}; //inisialisasi nilai array secara langsung
+        double[] number = {5.0, 12867, 7.5, 2000000}; //inisialisasi nilai array secara langsung
 
-        for (int i = 0; i <= 4; i++) {
+        for (int i = 0; i < 4; i++) {
             System.out.println(number[i]);
         }
     } 

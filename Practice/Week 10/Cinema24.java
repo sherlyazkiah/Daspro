4879b0f9bd27155a320d7b5f3d59c6263147c9a6
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-10 13:53:15 +0700

    display the audience’s name for each line

--- Practice/Week 10/Cinema24.java
+++ Practice/Week 10/Cinema24.java
@@ -15,11 +15,16 @@ public class Cinema24 {
         System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
 
+        //display the audience’s name for each line
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Audience in the row " + (i + 1) + ": " + String.join(", ", audience[i]));
+        }
+
         //display the audience’s name on the 3rd line using a foreach loop
-        System.out.println("Audiences in the row 3: ");
+        /*System.out.println("Audiences in the row 3: ");
         for (String i : audience[2]) {
             System.out.println(i);
-        }
+        }*/
 
         //display the audience’s name on the 3rd line using a for loop
         /*System.out.println("Audiences in the row 3: ");

5ff21be02f2281c546cbe285a147bf2554899e62
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-10 13:23:20 +0700

    display the length of each row in the array using foreach loop

--- Practice/Week 10/Cinema24.java
+++ Practice/Week 10/Cinema24.java
@@ -15,11 +15,16 @@ public class Cinema24 {
         System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
 
+        //display the length of each row in the array using foreach loop
+        for (String[] rowAudience : audience) {
+            System.out.println("Length of row: " + rowAudience.length);
+        }
+        
         //display the length of each row in the array using for loop
-        System.out.println(audience.length);
+        /*System.out.println(audience.length);
         for (int i = 0; i < audience.length; i++) {
             System.out.println("Length of row " + (i + 1) + ": " + audience[i].length);
-        }
+        }*/
         
         //audience.length gives the number of rows & audience[i].length gives the number of columns
         /*System.out.println(audience.length);

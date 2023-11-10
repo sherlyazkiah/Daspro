994c14136c57513149d5d13533c920d42e6047fb
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-10 13:40:00 +0700

    display the audience’s name on the 3rd line using a for loop

--- Practice/Week 10/Cinema24.java
+++ Practice/Week 10/Cinema24.java
@@ -15,10 +15,16 @@ public class Cinema24 {
         System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
 
-        //display the length of each row in the array using foreach loop
-        for (String[] rowAudience : audience) {
-            System.out.println("Length of row: " + rowAudience.length);
+        //display the audience’s name on the 3rd line using a for loop
+        System.out.println("Audiences in the row 3: ");
+        for (int i = 0; i < audience[2].length; i++) {
+            System.out.println(audience[2][i]);
         }
+
+        //display the length of each row in the array using foreach loop
+        /*for (String[] rowAudience : audience) {
+            System.out.println("Length of row: " + rowAudience.length);
+        }*/
         
         //display the length of each row in the array using for loop
         /*System.out.println(audience.length);

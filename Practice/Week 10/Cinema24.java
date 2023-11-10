55fc165a4445c8315df7240b438f46830a45ecf3
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-10 12:48:05 +0700

    complete the audience list

--- Practice/Week 10/Cinema24.java
+++ Practice/Week 10/Cinema24.java
@@ -8,6 +8,7 @@ public class Cinema24 {
         audience[2][0] = "Eka";
         audience[2][1] = "Farhan";
         audience[3][0] = "Gisel";
+        audience[3][1] = "Hana";
 
         System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
         System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);

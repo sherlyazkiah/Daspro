9abf8cb8214e49367d7a0bb2587f3e6f9bc0d245
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-14 22:03:08 +0700

    add menu

--- Practice/Week 10/CinemaWithScanner24.java
+++ Practice/Week 10/CinemaWithScanner24.java
@@ -3,27 +3,49 @@ import java.util.Scanner;
 public class CinemaWithScanner24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int row, column;
+        int row, column, choice;
         String name, next;
         String[][] audience = new String[4][2];
-
+        
         while (true) {
-            System.out.print("Enter a name: ");
-            name = sc.nextLine();
-            System.out.print("Enter row number: ");
-            row = sc.nextInt();
-            System.out.print("Enter column number: ");
-            column = sc.nextInt();
+            // display menu
+            System.out.println("Menu:");
+            System.out.println("1. Input audience data");
+            System.out.println("2. Show audience list");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice (1/2/3): ");
+            choice = sc.nextInt();
+            
             sc.nextLine();
 
-            audience[row - 1][column - 1] = name;
-            System.out.print("Are there any other audiences to be added? (y/n): ");
-            next = sc.nextLine();
+            if (choice == 1) {
+                while (true) {
+                    System.out.print("Enter a name: ");
+                    name = sc.nextLine();
+                    System.out.print("Enter row number: ");
+                    row = sc.nextInt();
+                    System.out.print("Enter column number: ");
+                    column = sc.nextInt();
+                    sc.nextLine(); 
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                    audience[row - 1][column - 1] = name;
+                    System.out.print("Are there any other audiences to be added? (y/n): ");
+                    next = sc.nextLine();
+
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }
+            } else if (choice == 2) {
+                for (int i = 0; i < audience.length; i++) {
+                    System.out.println("Audience in the row " + (i + 1) + ": " + String.join(", ", audience[i]));
+                }
+            } else if (choice == 3) {
+                sc.close();
+                System.exit(0);
+            } else {
+                System.out.println("Invalid choice. Please enter 1/2/3.");
             }
         }
-        sc.close();
     }
 }
---
+++ Practice/Week 9/Array2D.java
@@ -0,0 +1,16 @@
+public class Array2D {
+    public static void main(String[] args) {
+        int book[][] = new int[3][3];
+        book[0][0] = 2;
+        book[0][1] = 8;
+        book[0][2] = 6;
+        book[1][0] = 7;
+        book[1][1] = 3;
+        book[1][2] = 6;
+        book[2][0] = 4;
+        book[2][1] = 6;
+        book[2][2] = 5;
+
+        int book2[][] = {{2,8,6},{7,3,6},{4,6,5}};
+    }
+}

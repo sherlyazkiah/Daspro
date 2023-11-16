c5587a9a9df7fb8e157868c8785e6459bb145cfa
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-16 10:27:26 +0700

    new file

--- Practice/Week11/Star.java
+++ Practice/Week11/Star.java
@@ -2,7 +2,7 @@ package Week11;
 
 import java.util.Scanner;
 
-public class Star24 {
+public class Star {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");

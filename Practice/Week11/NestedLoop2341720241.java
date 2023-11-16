5fbf55c427e372e3154ecc7d17f20d4b6ac54fcb
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-16 15:16:20 +0700

    new file

---
+++ Practice/Week11/NestedLoop2341720241.java
@@ -0,0 +1,26 @@
+package Week11;
+
+import java.util.Scanner;
+
+public class NestedLoop2341720241 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[][] temps = new double[5][7];
+
+        for (int i = 0;i < temps.length; i++) {
+            System.out.println("Kota ke-" + (i + 1));
+            for (int j = 0;j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = sc.nextDouble();
+            }
+            System.out.println();
+        }
+        for (int i = 0;i < temps.length; i++) {
+            System.out.println("Kota ke-" + (i + 1) + ": ");
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print(temps[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

dd3f154db177b2ae2d631d139d085615955762c9
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-16 22:08:15 +0700

    modify: foreach loop data temperatur

--- Practice/Week11/NestedLoop.java
+++ Practice/Week11/NestedLoop.java
@@ -15,12 +15,23 @@ public class NestedLoop {
             }
             System.out.println();
         }
-        for (int i = 0;i < temps.length; i++) {
+
+        // menampilkan data temperatur menggunakan foreach loop
+        for (double[] cityTemps : temps) {
+            for (double temp : cityTemps) {
+                System.out.print(temp + " ");
+            }
+            System.out.println();
+        }
+
+        /*for (int i = 0;i < temps.length; i++) {
             System.out.println("Kota ke-" + (i + 1) + ": ");
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print(temps[i][j] + " ");
             }
             System.out.println();
-        }
-    }
+        }*/
+
+        sc.close();
+    } 
 }

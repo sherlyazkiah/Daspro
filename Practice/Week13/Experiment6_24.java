028f1f058e4f1ade3b8b71e7a01831ca87d99307
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-24 12:47:09 +0700

    add function

--- Practice/Week13/Experiment6_24.java
+++ Practice/Week13/Experiment6_24.java
@@ -3,6 +3,14 @@ package Week13;
 import java.util.Scanner;
 
 public class Experiment6_24 {
+    static int calculateArea (int l, int w) {
+        int area = l*w;
+        return area;
+    }
+    static int calculateVolume (int l, int w, int h) {
+        int vol = calculateArea(l, w)*h;
+        return vol;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int p, l, t, L, vol;
@@ -13,9 +21,9 @@ public class Experiment6_24 {
         System.out.print("Input height = ");
         t = sc.nextInt();
 
-        L = p*l;
+        L = calculateArea(p, l);
         System.out.println("Area of the rectangle = " + L);
-        vol = p*l*t;
+        vol = calculateVolume(t, p, l);
         System.out.println("Volume of the rectangle = " + vol);
     }
 }

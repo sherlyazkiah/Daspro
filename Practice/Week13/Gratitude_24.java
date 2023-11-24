c8b0ceb0000355b0bb4d39c8b51f50fb6b3d9264
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-24 10:14:25 +0700

    function with parameters

--- Practice/Week13/Gratitude_24.java
+++ Practice/Week13/Gratitude_24.java
@@ -5,7 +5,14 @@ public class Gratitude_24 {
         System.out.println("Thank you for being the best teacher in the world.\n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
+
+    public static void sayAdditionalGreetings(String greeting){
+        System.out.println(greeting);
+    }
+
     public static void main(String[] args) {
         sayThankyou();
+        String expression = "Thankyou...wish you all the best!";
+        sayAdditionalGreetings(expression);
     }
 }

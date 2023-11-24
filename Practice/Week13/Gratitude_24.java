cf9881f65507ce216a0aa66ed172e2a75ee264d5
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-24 07:57:01 +0700

    new file

---
+++ Practice/Week13/Gratitude_24.java
@@ -0,0 +1,11 @@
+package Week13;
+
+public class Gratitude_24 {
+    public static void sayThankyou() {
+        System.out.println("Thank you for being the best teacher in the world.\n" +
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+    public static void main(String[] args) {
+        sayThankyou();
+    }
+}

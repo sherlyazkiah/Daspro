603d70500bb27891e07eadbc55a81315c727c67c
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-24 07:32:30 +0700

    new file

---
+++ Practice/Week11/latihan.java
@@ -0,0 +1,14 @@
+package Week11;
+
+import javax.sound.midi.Soundbank;
+
+public class latihan {
+    public static void main(String[] args) {
+        int result=0;
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <= 3; j += 3) {
+            }
+        }
+        System.out.println(result);
+    }
+}

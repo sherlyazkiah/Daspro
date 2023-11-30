1221b77c4b950b6e722ae480dcfaf285c02c2329
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-11-30 22:50:29 +0700

    modify by add function sayAdditionalGreetings() with a String input parameter

--- Practice/Week13/ExpressingGratitude_24.java
+++ Practice/Week13/ExpressingGratitude_24.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ExpressingGratitude_24 {
     public static String getGreetingRecipient() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input the name of people you want to greet : ");
+        System.out.print("Input the name of people you want to greet: ");
         String recipientName = sc.nextLine();
         sc.close();
         return recipientName;
@@ -15,6 +15,10 @@ public class ExpressingGratitude_24 {
         String name = getGreetingRecipient();
         System.out.println("Thank you " + name + " for being the best teacher in the world.\n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void sayAdditionalGreetings(String additionalGreetings) {
+        System.out.println("Additional Greetings: " + additionalGreetings);
     }
 
     public static void main(String[] args) {
